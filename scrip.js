c4d214e9fe9de0fede01dd01632cb53066dff14c
Helena Rosa <helena.rosa@aluno.senai.br>
2026-04-09 11:34:42 -0300

    alteração da funcao um

---
+++ scrip.js
@@ -0,0 +1,36 @@
+const colorPicker = document.querySelector('#color-picker');
+const fontSelect = document.querySelector('#font-select');
+const themeToggle = document.querySelector('#theme-toggle');
+const articleColorPicker = document.querySelector('#aerticule-color-picker');
+const body = document.querySelector('body');
+const newsArticles = document.querySelectorAll('.news');
+
+// carrega as preferencias do usuario armazenadas no localStorage
+// @function loadPrefernces 
+
+const loadPreferences = () => {
+
+};
+
+colorPicker.addEventListener('input', (e) => {
+    const color = e.target.value; // capturar o movimento do mouse 
+    // mudar o valor da variavel no css, com o valor capturado
+    document.documentElement.style.setProperty('--primary-color', color)
+    //inseriri a chave (primaryColor) e o valor (color) no local storage 
+    localStorage.setItem('primaryColor', color);
+
+});
+
+articleColorPicker.addEventListener('input', (e) => {
+    const color = e.target.value;
+    newsArticles.forEach(article => {
+        article.style.backgroundColor = color;
+    });
+    localStorage.setItem('articleColor', color);
+});
+
+fontSelect.addEventListener('change', (e) => {
+});
+
+themeToggle.addEventListener('click', () => {
+});
